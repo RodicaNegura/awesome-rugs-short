8d2dda9eb631b5a1a13c92d6eb8b5165c8057ebc
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-09-21 20:08:05 +0100

    build and style section rugs and add more features

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var swiper = new Swiper(".rugs-slider", {
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-});
+// var swiper = new Swiper(".rugs-slider", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//     dynamicBullets: true,
+//   },
+// });
