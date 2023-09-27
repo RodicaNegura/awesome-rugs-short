1a2722c1e60bea15e53c6ba0318bf695ba99101e
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-09-27 12:30:50 +0100

    added login page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-// var swiper = new Swiper(".rugs-slider", {
-//   pagination: {
-//     el: ".swiper-pagination",
-//     dynamicBullets: true,
-//   },
-// });
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.nav-links')
+
+menu.onclick = ()  => {
+  menu.classList.toggle('fa-times')
+  navbar.classList.toggle('active')
+}
+
+document.querySelector('#login-btn').onclick = () => {
+  document.querySelector('.login-container').classList.toggle('active')
+}
+
+document.querySelector('#close-login-form').onclick = () => {
+  document.querySelector('.login-container').classList.remove('active')
+}
+
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.nav').classList.add('active')
+  } else {
+    document.querySelector('.nav').classList.add('active')
+  }
+
+  menu.classList.remove('fa-times')
+  navbar.classList.remove('active')
+}
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.nav').classList.add('active')
+  } else {
+    document.querySelector('.nav').classList.add('active')
+  }
+}
